d17f5d0f5bab4455078166e39c8c6de384c480ad
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-06-24 04:05:49 +0000

    add animation to the searbar toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./Route/MainRouter"
 import { MainProvider } from "./MainContext"
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,15 +1,23 @@
 import React, { useContext } from "react";
 import Layout from "../Core/Layout";
 import SearchBar from "../Core/SearchBar";
-import { MainContext } from "../../MainContext"
+import { MainContext } from "../../MainContext";
+
+import { useSpring, animated } from 'react-spring';
+
 
 const Home = () => {
-    const [showSearchBar, setShowSearchBar] = useContext(MainContext);
+
+    const [showSearchBar] = useContext(MainContext);
+    const contentProps = useSpring({
+        opacity: showSearchBar ? 1 : 0,
+        marginTop: showSearchBar ? 0 : -15
+    })
     return (
         <Layout>
-            {showSearchBar && (
+            <animated.div style={contentProps}>
                 <SearchBar />
-            )}
+            </animated.div>
         </Layout>
     )
 };
