d18f9e3e652163df590975cd9c894204c732371e
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-27 10:49:52 +0545

    add anchor to the actual link

--- src/Components/Core/Card.js
+++ src/Components/Core/Card.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const Card = React.memo(({ recipe }) => {
   return (
-    <div className="card mb-2" style={{ width: "22rem", height: "99%" }}>
-      <div className="card-header">
-        <p className="card-text text-center">{recipe.label}</p>
+    <a target="_blank" rel="noopner noreferer" href={recipe.url}>
+      <div className="card mb-2" style={{ width: "22rem", height: "99%" }}>
+        <div className="card-header">
+          <p className="card-text text-center">{recipe.label}</p>
+        </div>
+        {console.log(recipe)}
+        <img className="card-img-top" src={recipe.image} alt="Recipe" />
+        <div className="card-body">
+          <ul>
+            {recipe.ingredients.map((ingredient, i) => (
+              <li key={i}>{ingredient.text}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      {console.log(recipe)}
-      <img className="card-img-top" src={recipe.image} alt="Recipe" />
-      <div className="card-body">
-        <ul>
-          {recipe.ingredients.map((ingredient, i) => (
-            <li key={i}>{ingredient.text}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </a>
   );
 });
 export default Card;
